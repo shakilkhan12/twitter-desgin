a30a5f9121933ebcb09cae14c7000407c2fee95f
shakilkhan12 <shakilkhan621@gmail.com>
2020-08-11 08:51:56 +0500

    responsive design

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,30 @@
 import React from 'react'
-import { FaHome, FaHashtag, FaRegBell, FaRegEnvelope, FaRegBookmark, FaClipboardList, FaUserAlt, FaMehBlank } from 'react-icons/fa';
+import { FaHome, FaHashtag, FaRegBell, FaRegEnvelope, FaRegBookmark, FaClipboardList, FaUserAlt, FaMehBlank, FaBars } from 'react-icons/fa';
 import { AiOutlineTwitter } from 'react-icons/ai'
 
 const Sidebar = () => {
+    const [state, setState] = React.useState(true);
+    const toggle = () => {
+        setState(!state);
+    }
     return (
-        <ul className="sidebar">
-            <li>
-                <a href=""><AiOutlineTwitter className="icon logo" /></a>
-            </li>
-            <li><a href=""><FaHashtag className="icon logo" /><span>Explore</span></a></li>
-            <li><a href=""><FaRegBell className="icon" /><span>Notifications</span></a></li>
-            <li><a href=""><FaRegEnvelope className="icon" /><span>Messages</span></a></li>
-            <li><a href=""><FaRegBookmark className="icon" /><span>Bookmarks</span></a></li>
-            <li><a href=""><FaClipboardList className="icon" /><span>Lists</span></a></li>
-            <li><a href=""><FaUserAlt className="icon" /><span>Profile</span></a></li>
-            <li><a href=""><FaMehBlank className="icon" /><span>More</span></a></li>
-            <li><a href="" className="profileBtn">Tweet</a></li>
-        </ul >
+        <>
+            <span className="toggle" onClick={toggle}><FaBars /> </span>
+            {state ? (<ul className="sidebar">
+                <li>
+                    <a href=""><AiOutlineTwitter className="icon logo" /></a>
+                </li>
+                <li><a href=""><FaHashtag className="icon logo" /><span>Explore</span></a></li>
+                <li><a href=""><FaRegBell className="icon" /><span>Notifications</span></a></li>
+                <li><a href=""><FaRegEnvelope className="icon" /><span>Messages</span></a></li>
+                <li><a href=""><FaRegBookmark className="icon" /><span>Bookmarks</span></a></li>
+                <li><a href=""><FaClipboardList className="icon" /><span>Lists</span></a></li>
+                <li><a href=""><FaUserAlt className="icon" /><span>Profile</span></a></li>
+                <li><a href=""><FaMehBlank className="icon" /><span>More</span></a></li>
+                <li><a href="" className="profileBtn">Tweet</a></li>
+            </ul >) : ''}
+
+        </>
     )
 }
 
